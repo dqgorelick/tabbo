59247739217ad998e73b2680aa077790e9fb19f1
Jun Woo Shin <shin.junwoo97@gmail.com>
2019-01-27 19:57:22 -0500

    extension testing is wack i give up for now

--- tests/unit/utils.spec.ts
+++ tests/unit/utils.spec.ts
@@ -1,10 +1,11 @@
 'use strict';
 
-//import * as utils from './src/utils';
+import * as utils from '../../src/scripts/utils';
 
 
-describe('out', (): void => {
-	test('test', (): void => {
-		expect(1 + 2).toBe(3);
+describe('queryOrThrow', (): void => {
+	test('queries', (): void => {
+		const prom = utils.queryOrThrow('#name');
+		expect(typeof(prom)).toBe(Promise);
 	});
 });
