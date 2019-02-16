02562b9852d439874daba74eb7d361b7d29330e1
Jun Woo Shin <shin.junwoo97@gmail.com>
2019-02-15 23:32:03 -0500

    make different sections of building more noticeable

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,33 +2,48 @@
 
 #set -eu
 
-echo 'Running webpack...'
-npx webpack
+function textBorder() {
+	local STR="$*"
+	local LEN=${#STR}
+	local TOP_BOTTOM=""
+
+	for (( INDEX=0; INDEX < LEN + 4; INDEX++ )); do
+		TOP_BOTTOM+='-'
+	done
+
+	printf '%b\n' "${TOP_BOTTOM}\n| ${STR} |\n${TOP_BOTTOM}"
+}
+
+textBorder 'Running webpack...'
+printf '%b\n' "$TEXT_BORDER_RET"
+npx webpack 2>&1 | tee -a $LOG
 
 if [[ ! -d ./dist ]]; then
 	mkdir -p dist/unpacked/scripts
 	mkdir -p dist/packed
 fi
 
-echo 'Copying HTML files...'
+textBorder 'Copying HTML files...'
 cp -r src/*.html dist/unpacked/
 
-echo 'Copying image files...'
+textBorder 'Copying image files...'
 cp -r src/images dist/unpacked/
 
-echo 'Copying style files...'
+textBorder 'Copying style files...'
 cp -r src/styles dist/unpacked/
 
-echo 'Copying manifest...'
+textBorder 'Copying manifest...'
 cp -r manifest.json dist/unpacked/
 
-echo 'Copying bundled js files...'
+textBorder 'Copying bundled js files...'
 cp -r dist/scripts/* dist/unpacked/scripts/
 
-echo 'Packaging extension...'
+textBorder 'Packaging extension...'
 pushd dist/unpacked
 zip -r --exclude=*.DS_Store* -FS ../packed/tabbo.zip *
 popd
 
-echo 'Linting...'
+textBorder 'Linting...'
 npx addons-linter dist/packed/tabbo.zip
+
+printf 'Log file: %s\n' $LOG
