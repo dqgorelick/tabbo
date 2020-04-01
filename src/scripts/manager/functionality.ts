df2c1c5f801f8b4ee990b461ca0b965a13042251
Jun Woo Shin <shin.junwoo97@gmail.com>
2020-03-31 21:20:00 -0700

    Add back screenshot functionality to the manager

--- src/scripts/manager/functionality.ts
+++ src/scripts/manager/functionality.ts
@@ -1,36 +1,28 @@
 'use strict';
 
-import * as logging from '../logging';
 import * as utils from '../utils';
 
 import {browser} from 'webextension-polyfill-ts';
 
 
 export const moveTabToWindow = async (w: browser.windows.Window, t: browser.tabs.Tab): Promise<void> => {
-	browser.tabs.move(t.id, {windowId: w.id, index: -1}).catch(
-		(e: Error): void => {
-			logging.error(e.message);
-		}
-	);
+	if (w.id === undefined || t.id === undefined) {
+		return;
+	}
 
-	browser.windows.update(w.id, {focused: true}).catch(
-		(e: Error): void => {
-			logging.error(e.message);
-		}
-	);
-
-	browser.tabs.update(t.id, {active: true, pinned: t.pinned}).catch(
-		(e: Error): void => {
-			logging.error(e.message);
-		}
-	);
+	await browser.tabs.move(t.id, {windowId: w.id, index: -1});
+	await browser.windows.update(w.id, {focused: true});
+	await browser.tabs.update(t.id, {active: true, pinned: t.pinned});
 };
 
 
 export const main = async (sendTabID: number): Promise<void> => {
-	const windows: browser.windows.Window[] = await browser.windows.getAll({populate: true});
 	const current: browser.windows.Window = await browser.windows.getCurrent();
-	let count: number = 1;
+	const windows: browser.windows.Window[] = (
+		await browser.windows.getAll({populate: true})
+	).filter((w: browser.windows.Window): boolean => {
+		return current.id !== w.id;
+	});
 
 	const openWindowsElement: HTMLElement = utils.queryOrThrow('#open-windows');
 	const t: browser.tabs.Tab = await browser.tabs.get(sendTabID);
@@ -42,39 +34,49 @@ export const main = async (sendTabID: number): Promise<void> => {
 		openWindowsElement.appendChild(h1);
 	}
 
-	windows.forEach((w: browser.windows.Window): void => {
-		if (w.id === current.id) {
-			return;
-		}
+	const screenshots: string[] = await Promise.all(windows.map((w: browser.windows.Window): Promise<string> => {
+		return browser.tabs.captureVisibleTab(w.id, {
+			format: 'jpeg',
+			quality: 30,
+		});
+	}));
 
-		const windowNum = count.toString();
-		utils.queryOrThrow('body').addEventListener(
-			'keydown',
-			(e: KeyboardEvent) => {
-				if (e.key === windowNum) {
-					moveTabToWindow(w, t);
-				}
-			}
-		);
+	windows.forEach(async (w: browser.windows.Window, i: number): Promise<void> => {
+		const index = (i + 1).toString();
 
 		if (w.tabs) {
 			const div: HTMLDivElement = document.createElement('div');
 			div.className = 'screenshot';
+			div.style.backgroundImage = `url(${screenshots[i]})`;
 			div.innerHTML = `<div class="title-bar"> <img src="${w.tabs[0].favIconUrl}"/>
 				<div class="screen-title">${w.tabs[0].title}</div></div>
-				<div class="screen-index">${count}</div>
+				<div class="screen-index">${index}</div>
 				<div class="tab-count">${w.tabs.length + (w.tabs.length === 1 ? " tab" : " tabs")}
 				</div>`;
 
-			div.addEventListener('click', (): void => {
-				moveTabToWindow(w, t);
+			div.addEventListener('click', async (): Promise<void> => {
+				await moveTabToWindow(w, t);
 			});
 
 			openWindowsElement.appendChild(div);
 		}
-
-		count++;
 	});
+
+	utils.queryOrThrow('body').addEventListener(
+		'keydown',
+		async (e: KeyboardEvent) => {
+			const index: number = parseInt(e.key) - 1;
+			if (isNaN(index)) {
+				return;
+			}
+
+			if (index < 0 || index >= windows.length) {
+				return;
+			}
+
+			await moveTabToWindow(windows[index], t);
+		}
+	);
 
 	utils.queryOrThrow('#current-tab').innerHTML = `<div class="title-bar"><img src="${t.favIconUrl}"/>` +
 		`<div class="screen-title">${t.title}</div></div>`;
