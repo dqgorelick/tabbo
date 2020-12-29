91f8cf1272a1207119e817f6119272efe9d9d362
Jun Woo Shin <shin.junwoo97@gmail.com>
2020-12-28 22:37:14 -0800

    configuration first pass

--- src/scripts/popup/index.ts
+++ src/scripts/popup/index.ts
@@ -10,34 +10,6 @@ import {browser} from 'webextension-polyfill-ts';
 const port: browser.runtime.Port = browser.runtime.connect();
 
 
-/* FIXME
-* Chrome doesn't support `browser.runtime.getBrowserInfo` so we can't use that!
-* When it does, change to that.
-*/
-/*
- *utils.queryOrThrow('#keybinds').addEventListener('click', (): void => {
- *  const browserType: utils.Browser = utils.checkBrowser();
- *  if (browserType === utils.Browser.CHROME) {
- *    // Chrome
- *    port.postMessage({action: tabbo.PopUpCommand.CHROME_KEYBINDS});
- *  } else if (browserType === utils.Browser.FIREFOX) {
- *    // Firefox
- *    port.postMessage({action: tabbo.PopUpCommand.KEYBINDS});
- *  } else {
- *    // FIXME
- *    alert('Invalid browser - only Chrome and Firefox are supoprted');
- *  }
- *});
- */
-
-/*
- *utils.queryOrThrow('#instructions').addEventListener('click', (): void => {
- *  port.postMessage({action: tabbo.PopUpCommand.INSTRUCTIONS});
- *});
- */
-
- // TODO add functionality for settings
-
 utils.queryOrThrow('#pop').addEventListener('click', (): void => {
 	port.postMessage({action: tabbo.PopUpCommand.POP_TAB});
 });
@@ -50,4 +22,28 @@ utils.queryOrThrow('#send').addEventListener('click', (): void => {
 
 utils.queryOrThrow('#join').addEventListener('click', (): void => {
 	port.postMessage({action: tabbo.PopUpCommand.GATHER_WINDOW});
+});
+
+
+/* FIXME
+* Chrome doesn't support `browser.runtime.getBrowserInfo` so we can't use that!
+* When it does, change to that.
+*/
+utils.queryOrThrow('#settings').addEventListener('click', (): void => {
+	const browserType: utils.Browser = utils.checkBrowser();
+	if (browserType === utils.Browser.CHROME) {
+		// Chrome
+		port.postMessage({action: tabbo.PopUpCommand.CHROME_KEYBINDS});
+	} else if (browserType === utils.Browser.FIREFOX) {
+		// Firefox
+		port.postMessage({action: tabbo.PopUpCommand.KEYBINDS});
+	} else {
+		// FIXME
+		alert('Invalid browser - only Chrome and Firefox are supoprted');
+	}
+});
+
+
+utils.queryOrThrow('#help').addEventListener('click', (): void => {
+	port.postMessage({action: tabbo.PopUpCommand.HELP});
 });
